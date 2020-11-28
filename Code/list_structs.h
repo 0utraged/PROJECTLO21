42840734dfd8e676644af24b1eaf5b8236470fc8
0utraged <58376872+0utraged@users.noreply.github.com>
2020-11-28 18:52:57 +0100

    Added core files

---
+++ Code/list_structs.h
@@ -0,0 +1,26 @@
+#ifndef LIST_STRUCTS
+#define LIST_STRUCTS
+
+typedef struct elem {
+
+  int value ;
+  struct elem * next ;
+  struct elem * prev ;
+
+} ListElement ;
+
+/*List est un pointeur vers une liste d'éléments de structure ListElement*/
+typedef ListElement * List ;
+
+/*La structure LOL (List of Lists) est une liste d'adresses vers des listes d'éléments de structure ListElement*/
+typedef struct listoflists {
+
+  List Listptr;
+  struct listoflists* next ;
+  struct listoflists* prev ;
+
+} LOL ;
+
+typedef LOL * LOLptr ;
+
+#endif
---
+++ Code/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "list_structs.h"
+
+int main(){
+
+
+
+
+  return 0;
+}
