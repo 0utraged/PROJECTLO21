50238eb0153e77f811112a5f6c0989e00040074f
0utraged <58376872+0utraged@users.noreply.github.com>
2020-12-30 18:02:24 +0100

    IT'S THE UTLTIMATE FINAL COMMIT ON HEAVEN'S DOORS OF GREATER ACHIE

--- Code/fileio.h
+++ Code/fileio.h
@@ -9,11 +9,16 @@
 #include "premisse.h"
 #include "regle.h"
 #include "baseconnaissance.h"
-/*La structure Regle (List of Lists) est une liste d'adresses vers des listes d'éléments de structure ListElement*/
 
-//Prototypes de fonctions:
+/** lit le contenu d'un fichier caractère par caractère. Lorsqu'un '/' est lu, une nouvelle règle (active) est créée, lorsqu'un '&' est lu, le mot est ajouté à la prémisse de la règle active
+    lorsqu'un ':' est lu, le mot devient la conclusion de la règle active.
+*/
 BC read_from_file(BC b);
 
+/** Copie le contenu de la base de connaissances dans un fixhier texte. Lorsqu'on qu'on passe à la règle suivante, un '/' est écrit dans le fichier,
+    le contenu de l'élément et un '&' sont écrit dans le fichier
+    Si la règle possède une conclusion, le contenu de la conclusion et un ':' sont écrit dans le fichier
+*/
 void write_to_file(BC * b);
 
 #endif
