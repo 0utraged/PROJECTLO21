c14575f7ec5f890a604309bf5c8217b35e6b5c6a
0utraged <58376872+0utraged@users.noreply.github.com>
2020-12-30 15:39:30 +0100

    mini update

--- Code/engine.c
+++ Code/engine.c
@@ -138,7 +138,7 @@ int menu_bf(){
             b=create_regle(b);
 
         }
-        printf("\n What do you want to do? Enter the proper number:\n 1:Create a new empty knowledge base.\n 2:Delete the knowledge base.\n 3:Display the entire knowledge base.\n 4:Add a new empty rule.\n 5:Display a rule thanks to its ID.\n 6:Delete a rule thanks to its ID.\n 7:Delete the premise of a rule thanks to its ID.\n 8:Add a proposition to the premise of a rule thanks to its ID.\n 9:Add or change the conclusion of a rule thanks to its ID.\n 10:Check if a proposition is in the premise of a rule thanks to its ID.\n 11:Check if the premise of a rule is empty thanks to its ID.\n 12:Display the conclusion of a rule thanks to its ID.\n 13:Display the premise of a rule thanks to its ID.\n 14:Delete a proposition from the premise of a rule thanks to its ID.\n 15:Display the first proposition of a premise thanks to the ID of the rule.\n 16:Start the engine and begin to give facts.\n 17:Save your changes to the knowledge base.\n 18:Read from database file.\n 19:Exit program.\n ");
+        printf("\n What do you want to do? Enter the proper number:\n 1:Create a new empty knowledge base.\n 2:Delete the knowledge base.\n 3:Display the entire knowledge base.\n 4:Add a new empty rule.\n 5:Display a rule thanks to its ID.\n 6:Delete a rule thanks to its ID.\n 7:Delete the premise of a rule thanks to its ID.\n 8:Add a proposition to the premise of a rule thanks to its ID.\n 9:Add or change the conclusion of a rule thanks to its ID.\n 10:Check if a proposition is in the premise of a rule thanks to its ID.\n 11:Check if the premise of a rule is empty thanks to its ID.\n 12:Display the conclusion of a rule thanks to its ID.\n 13:Display the premise of a rule thanks to its ID.\n 14:Delete a proposition from the premise of a rule thanks to its ID.\n 15:Display the first proposition of a premise thanks to the ID of the rule.\n 16:Start the engine and begin to give facts.\n 17:Save your changes to the knowledge base.\n 18:Read from database file.\n 19:Exit program.\n 20:Display head rule of database \n");
       scanf("%d",&menu);
        //TODO: IF BASE EMPTY THEN FORCE CREATE IT
       switch(menu){
@@ -281,6 +281,9 @@ int menu_bf(){
                 printf("Exiting program.\n");
                 menu = 99;
             }
+        break;
+        case 20:
+            display_regle(b);
         break;
         default:
 
